38466c9bd47d261b001f6881270b25219871353e
Prashant Shrivastava <tera-real-email@example.com>
2025-05-08 02:03:11 +0530

    e

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
+
 );
